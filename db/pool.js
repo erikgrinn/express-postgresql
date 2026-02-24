9d7d3eed38c40a91f7420e17c5068ae052c11a0c
Erik Grinn <grinn.erik@gmail.com>
2026-02-24 10:02:51 -0500

    shebang file (psql client). env variables

--- db/pool.js
+++ db/pool.js
@@ -1,18 +1,14 @@
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "ejgri",
+  host: "localhost", 
+  user: process.env.DB_USER,
   database: "top_users",
-  password: process.env.DATABASE_PASSWORD,
-  port: 5432 // The default port
+  password: process.env.DB_PASSWORD,
+  port: 5432, // The default port
 });
 
-// const { Pool } = require("pg");
-
-// // Again, this should be read from an environment variable
+// or
 // module.exports = new Pool({
 //   connectionString: "postgresql://<role_name>:<role_password>@localhost:5432/top_users"
 // });
